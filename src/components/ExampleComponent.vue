3cc5b4c21fd077f03ed4fa4092f8eda3e064cb15
Daniil <eddanya@gmail.com>
2025-04-10 00:56:46 +0300

    first commit

--- src/components/ExampleComponent.vue
+++ src/components/ExampleComponent.vue
@@ -1,37 +1,73 @@
 <template>
-  <div>
-    <p>{{ title }}</p>
-    <ul>
-      <li v-for="todo in todos" :key="todo.id" @click="increment">
-        {{ todo.id }} - {{ todo.content }}
-      </li>
-    </ul>
-    <p>Count: {{ todoCount }} / {{ meta.totalCount }}</p>
-    <p>Active: {{ active ? 'yes' : 'no' }}</p>
-    <p>Clicks on todos: {{ clickCount }}</p>
+  <div class="q-pa-md full-width">
+
+    <q-form
+      @submit="onSubmit"
+      @reset="onReset"
+      class="q-gutter-md"
+    >
+      <q-input
+        filled
+        rounded
+        v-model="name"
+        label="Электронная почта"
+        lazy-rules
+        :rules="[ val => val && val.length > 0 || 'Please type something']"
+      />
+
+      <q-input
+        filled
+        rounded
+        type="number"
+        v-model="age"
+        label="Пароль"
+        lazy-rules
+        :rules="[
+          val => val !== null && val !== '' || 'Please type your age',
+          val => val > 0 && val < 100 || 'Please type a real age'
+        ]"
+      />
+<!--      <q-toggle v-model="accept" label="I accept the license and terms"/>-->
+
+      <div class="flex justify-center">
+        <q-btn rounded label="Войти" type="submit" color="primary"/>
+      </div>
+    </q-form>
+
   </div>
 </template>
 
 <script setup lang="ts">
-import { computed, ref } from 'vue';
-import type { Todo, Meta } from './models';
+import {useQuasar} from 'quasar'
+import {ref} from 'vue'
 
-interface Props {
-  title: string;
-  todos?: Todo[];
-  meta: Meta;
-  active: boolean;
-};
+const $q = useQuasar()
 
-const props = withDefaults(defineProps<Props>(), {
-  todos: () => []
-});
+const name = ref(null)
+const age = ref(null)
+const accept = ref(false)
 
-const clickCount = ref(0);
-function increment() {
-  clickCount.value += 1;
-  return clickCount.value;
+function onSubmit() {
+  if (accept.value !== true) {
+    $q.notify({
+      color: 'red-5',
+      textColor: 'white',
+      icon: 'warning',
+      message: 'You need to accept the license and terms first'
+    })
+  } else {
+    $q.notify({
+      color: 'green-4',
+      textColor: 'white',
+      icon: 'cloud_done',
+      message: 'Submitted'
+    })
+  }
 }
 
-const todoCount = computed(() => props.todos.length);
+function onReset() {
+  name.value = null
+  age.value = null
+  accept.value = false
+}
 </script>
